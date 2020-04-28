0e45ecd31de82b71e4275e88b63f5dfa61711381
asthajain99 <46923265+asthajain99@users.noreply.github.com>
2020-04-28 15:56:11 +0530

    Add files via upload

---
+++ 92.HelloWorldWithoutSemicolon.cpp
@@ -0,0 +1,15 @@
+//Write a program to print hello world without using semicolon
+#include<iostream>
+using namespace std;
+int main()
+{
+	if(cout<<"Hello World")
+	{}
+	return 0;
+}
+
+//OUTPUT
+//Hello World
+//--------------------------------
+//Process exited after 0.1492 seconds with return value 0
+//Press any key to continue . . .
